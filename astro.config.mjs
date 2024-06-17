1303f88d2d681d39e2f10d0122a5950ec87be9f5
Onrirr <ulughann@gmail.com>
2024-06-17 20:20:39 +0300

    this part is good

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,30 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      plugins: [
+        starlightUtils({
+          multiSidebar: {
+            switcherStyle: "horizontalList",
+          },
+        }),
+      ],
+      title: "Modern Standart Turkic",
+      customCss: ["./src/styles/global.css"],
+      sidebar: [
+        {
+          label: "Morphology",
+          autogenerate: { directory: "morphology" },
+        },
+        {
+          label: "Dictionary",
+          autogenerate: { directory: "dictionary" },
+        },
+      ],
+    }),
+  ],
 });
